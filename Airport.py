7834e5c8b336f4b8a7badd76deaf6e8e91927c8a
olcastan <o_castaneda@outlook.com>
2019-07-11 12:49:05 -0500

    Delete Airport.py

--- Airport.py
+++
@@ -1,8 +0,0 @@
-class Airport:
-	def __init__(self, name, code, city, state, temp, delay):
-		self.name = name
-		self.code = code
-		self.city = city
-		self.state = state
-		self.temp = temp
-		self.delay = delay
